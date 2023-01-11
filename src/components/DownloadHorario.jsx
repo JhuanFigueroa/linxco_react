1cea19dcccf02b13bd22e0058947aa041a62c00c
jhuanfigueroa <figueroatrejojuan1h@gmail.com>
2023-01-10 20:54:02 -0600

    Descargar horarios

--- src/components/DownloadHorario.jsx
+++ src/components/DownloadHorario.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import '../styles/Horarios.scss'
 import gif from '@imagenes/hor.gif'
+import {useAuth} from "../hooks/useAuth";
+import axios from "axios";
+let Documento
 const DownloadHorario=()=>{
+    let claveCarrera=0
+    const auth=useAuth()
+    const user = auth.user;
+
+    const obtnenerClaveCarrera= async ()=>{
+        const rta= await axios.get('http://localhost:3000/api/v1/alumnos/'+user.clave)
+        claveCarrera=rta.data.claveCarrera
+        const res=await axios.get('http://localhost:3000/api/v1/horarios/carrera/'+claveCarrera)
+    }
+
+    useEffect(()=>{
+        obtnenerClaveCarrera()
+
+
+    },[])
+
     return(
         <section className="ContentHorariosJefes">
             <div className="horariosJ">
                 <h2 className="textHJ">Descargar Archivo</h2><br/>
-                <button className="descargaHA btn-outline-primary"><img src={gif} width="100%"
-                                                                        height="100%"/></button>
+
+
+                <a href={Documento} download> <button className="descargaHA btn-outline-primary"><img src={gif} width="100%"
+                                                                                                                            height="100%"/></button></a>
             </div>
         </section>
     );
